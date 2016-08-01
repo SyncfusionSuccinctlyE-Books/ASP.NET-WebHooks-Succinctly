bfb7588cd1cdbcdd1708871fbaa5715c91aa5045
Gaurav Arora <g_arora@hotmail.com>
2016-08-01 23:01:27 +0530

    added chapter example,including images

---
+++ Chapter-02/BitBucketSampleReceiverApp/BitBucketSampleReceiverApp/WebHooks/BitBucketWebHookhandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using BitBucketSampleReceiverApp.Constants;
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+
+namespace BitBucketSampleReceiverApp.WebHooks
+{
+    public class BitBucketWebHookhandler : WebHookHandler
+    {
+        public BitBucketWebHookhandler()
+        {
+            Receiver = BitbucketWebHookReceiver.ReceiverName;
+        }
+
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            var dataJObject = context.GetDataOrDefault<JObject>();
+            var action = context.Actions.First();
+            switch (action)
+            {
+                case BitBucketRepoAction.Push:
+                    var repository = dataJObject["repository"].ToObject<BitbucketRepository>();
+                    var actor = dataJObject["actor"].ToObject<BitbucketUser>();
+                    AssessChanges(dataJObject);
+                    break;
+
+                default:
+                    var data = dataJObject.ToString();
+                    break;
+            }
+            return Task.FromResult(true);
+        }
+
+        private static void AssessChanges(JObject dataJObject)
+        {
+            foreach (var change in dataJObject["push"]["changes"])
+            {
+                var previousValue = change["old"]["target"].ToObject<BitbucketTarget>();
+
+                var newValue = change["new"]["target"].ToObject<BitbucketTarget>();
+            }
+        }
+    }
+}
---
+++ Chapter-02/BitBucketSampleReceiverApp/BitBucketSampleReceiverApp/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BitBucketSampleReceiverApp.WebApiApplication" Language="C#" %>
