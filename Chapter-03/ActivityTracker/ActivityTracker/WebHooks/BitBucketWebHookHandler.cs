21070b46a7f25fd4b8b0a7987fcfa1fe9ac80ed4
Gaurav Arora <g_arora@hotmail.com>
2016-08-06 19:51:38 +0000

    implemented common functionality of BitbucketWebHookHandler class

--- Chapter-03/ActivityTracker/ActivityTracker/WebHooks/BitBucketWebHookHandler.cs
+++ Chapter-03/ActivityTracker/ActivityTracker/WebHooks/BitBucketWebHookHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ActivityTracker.Helper;
+using Microsoft.AspNet.WebHooks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace ActivityTracker.WebHooks
 {
-    class BitBucketWebHookHandler
+    public class BitBucketWebHookHandler : WebHookHandler
     {
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            if (Common.IsBitBucketReceiver(receiver))
+            {
+
+            }
+
+            return Task.FromResult(true);
+        }
     }
 }
