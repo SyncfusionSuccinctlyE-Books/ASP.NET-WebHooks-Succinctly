72520f6a4eb483d2f1c35f3f04ecaae7ae493a10
Gaurav Arora <g_arora@hotmail.com>
2016-08-07 13:33:39 +0000

    added activity model

---
+++ Chapter-03/ActivityTracker/ActivityTracker/Models/ActivityModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ActivityTracker.Models
+{
+    public class ActivityModel
+    {
+        public string Activity { get; set; }
+        public string Action { get; set; }
+        public string Description { get; set; }
+        public string Data { get; set; }
+    }
+}
