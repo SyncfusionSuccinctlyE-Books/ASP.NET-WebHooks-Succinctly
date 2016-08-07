ea92ed3c29b29c641a38f1e1a7965aaef4c6e920
Gaurav Arora <g_arora@hotmail.com>
2016-08-08 00:40:55 +0530

    added repository

--- Chapter-03/ActivityTracker/ActivityTracker/Enums/EnumRepository.cs
+++ Chapter-03/ActivityTracker/ActivityTracker/Enums/EnumRepository.cs
@@ -4,24 +4,11 @@ namespace ActivityTracker.Enums
 {
     public enum EnumRepository
     {
-        [EnumDisplayName("Changes pushed to remote")]
-        [EnumDisplayCode("repo:push")]
-        push,
-        [EnumDisplayName("Fork")]
-        [EnumDisplayCode("repo:fork")]
-        fork,
-        [EnumDisplayName("Updated")]
-        [EnumDisplayCode("repo:updated")]
-        updated,
-        [EnumDisplayName("Commit comment created")]
-        [EnumDisplayCode("repo:commit_comment_created")]
-        commitcommentcreated,
-        [EnumDisplayName("Commit status created")]
-        [EnumDisplayCode("repo:commit_status_created")]
-        commitstatuscreated,
-        [EnumDisplayName("Commit status updated")]
-        [EnumDisplayCode("repo:commit_status_updated")]
-        commitstatusupdated
-
+        [EnumDisplayName("Changes pushed to remote")] [EnumDisplayCode("repo:push")] Push,
+        [EnumDisplayName("Fork")] [EnumDisplayCode("repo:fork")] Fork,
+        [EnumDisplayName("Updated")] [EnumDisplayCode("repo:updated")] Updated,
+        [EnumDisplayName("Commit comment created")] [EnumDisplayCode("repo:commit_comment_created")] Commitcommentcreated,
+        [EnumDisplayName("Commit status created")] [EnumDisplayCode("repo:commit_status_created")] Commitstatuscreated,
+        [EnumDisplayName("Commit status updated")] [EnumDisplayCode("repo:commit_status_updated")] Commitstatusupdated
     }
 }
--- Chapter-03/ActivityTracker/ActivityTracker/WebHooks/BitBucketWebHookHandler.cs
+++ Chapter-03/ActivityTracker/ActivityTracker/WebHooks/BitBucketWebHookHandler.cs
@@ -1,16 +1,24 @@
-﻿using ActivityTracker.Constants;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using ActivityTracker.Enums;
 using ActivityTracker.Helper;
 using ActivityTracker.Helpers;
+using ActivityTracker.Models;
+using ActivityTracker.Persistance;
 using Microsoft.AspNet.WebHooks;
 using Newtonsoft.Json.Linq;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ActivityTracker.WebHooks
 {
     public class BitBucketWebHookHandler : WebHookHandler
     {
+        private readonly ActivityRepository _activityRepository;
+
+        public BitBucketWebHookHandler()
+        {
+            _activityRepository = new ActivityRepository();
+        }
+
         public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
         {
             if (Common.IsBitBucketReceiver(receiver))
@@ -20,22 +28,30 @@ namespace ActivityTracker.WebHooks
                 var enumAction = EnumHelper.GetEnumValue<EnumRepository>(action, EnumProp.DisplayCode, false);
                 switch (enumAction)
                 {
-                    case EnumRepository.push:
+                    case EnumRepository.Push:
+                        var activityModel = new ActivityModel
+                        {
+                            Activity = "Repository",
+                            Action = EnumHelper.GetEnumDisplayName(EnumRepository.Push, EnumProp.DisplayCode),
+                            Description = EnumHelper.GetEnumDisplayName(EnumRepository.Push,
+                                EnumProp.DisplayName),
+                            Data = dataJObject.ToString()
+                        };
+                        _activityRepository.Add(activityModel);
+                        break;
+                    case EnumRepository.Fork:
                         //do something
                         break;
-                    case EnumRepository.fork:
+                    case EnumRepository.Updated:
                         //do something
                         break;
-                    case EnumRepository.updated:
+                    case EnumRepository.Commitcommentcreated:
                         //do something
                         break;
-                    case EnumRepository.commitcommentcreated:
+                    case EnumRepository.Commitstatuscreated:
                         //do something
                         break;
-                    case EnumRepository.commitstatuscreated:
-                        //do something
-                        break;
-                    case EnumRepository.commitstatusupdated:
+                    case EnumRepository.Commitstatusupdated:
                         //do something
                         break;
 
@@ -43,11 +59,9 @@ namespace ActivityTracker.WebHooks
                         var data = dataJObject.ToString();
                         break;
                 }
-
             }
 
             return Task.FromResult(true);
         }
-
     }
 }
