8b77fa48700dc3d5c9364d7203fd282a2006f033
Gaurav Arora <g_arora@hotmail.com>
2016-08-08 16:08:01 +0530

    Added angularjs controler Added Angularjs Nuget packages

---
+++ Chapter-03/ActivityTracker/ActivityTracker/Scripts/app/controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('controller', controller);
+
+    controller.$inject = ['$location']; 
+
+    function controller($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'controller';
+
+        activate();
+
+        function activate() { }
+    }
+})();
