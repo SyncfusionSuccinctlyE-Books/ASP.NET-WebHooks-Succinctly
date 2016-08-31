09117c3df9e86faeda162da292f4bea7c302079e
Gaurav Arora <gaurav.arora6@gmail.com>
2016-08-31 22:22:42 +0000

    completing example

---
+++ Chapter-07/TipsTricks/src/ImplementingDIandLogger/obj/Debug/Package/PackageTmp/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using log4net.Config;
+
+namespace ImplementingDIandLogger
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new {id = RouteParameter.Optional}
+            );
+            XmlConfigurator.Configure();
+
+
+            //WebHook initialization
+            config.InitializeReceiveBitbucketWebHooks();
+        }
+    }
+}
